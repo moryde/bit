49faa5c86a1d922a88db750d05cda9d338f35c29
Morten Ydefeldt <morten@ydefeldt.com>
2014-03-16 20:51:51 +0100

    Initial Commit

---
+++ Beep/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Beep
+//
+//  Created by Morten Ydefeldt on 16/03/14.
+//  Copyright (c) 2014 Ydefeldt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
